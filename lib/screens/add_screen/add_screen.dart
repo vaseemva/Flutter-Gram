3ab2569fce44fc8471305186b03290ac02242db7
Vaseem Anwar <vaseemanwarafi@gmail.com>
2023-01-31 20:23:10 +0530

    add options created

--- lib/screens/add_screen/add_screen.dart
+++ lib/screens/add_screen/add_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gram/screens/add_screen/widgets/add_option.dart';
+import 'package:flutter_gram/utils/constants.dart';
 
 class AddScreen extends StatelessWidget {
-const AddScreen({ Key? key }) : super(key: key);
+  const AddScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return const Center(
-      child: Text('add'),
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: AddOption(
+              size: size,
+              iconData: Icons.article,
+              label: 'Add Article',
+            ),
+          ),
+          kHeight20,
+          Center(
+            child: AddOption(
+                size: size, label: 'Add Event', iconData: Icons.event),
+          )
+        ],
+      ),
     );
   }
 }
