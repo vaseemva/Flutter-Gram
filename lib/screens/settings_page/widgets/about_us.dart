bdf7261d48647685dc10496b8697a3c4eff57021
Vaseem Anwar <vaseemanwarafi@gmail.com>
2023-03-01 17:30:02 +0530

    privacy policy title added

--- lib/screens/settings_page/widgets/about_us.dart
+++ lib/screens/settings_page/widgets/about_us.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gram/screens/settings_page/widgets/settings_tile.dart';
+import 'package:flutter_gram/utils/constants.dart';
 import 'package:flutter_gram/utils/strings.dart';
 
 class AboutUs extends StatelessWidget {
@@ -11,12 +12,25 @@ class AboutUs extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        showModalBottomSheet(context: context, builder: (context) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(aboutMeDescription), 
-          );
-        },);
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  const Text(
+                    "About Us",
+                    style:
+                        TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold),
+                  ),
+                  kHeight10,
+                  Text(aboutMeDescription),
+                ],
+              ),
+            );
+          },
+        );
       },
       child: SettingsTile(
         title: tileTitle6,
--- lib/screens/settings_page/widgets/privacy_policy.dart
+++ lib/screens/settings_page/widgets/privacy_policy.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gram/screens/settings_page/widgets/settings_tile.dart';
+import 'package:flutter_gram/utils/constants.dart';
 import 'package:flutter_gram/utils/strings.dart';
 
 class PrivacyPolicy extends StatelessWidget {
@@ -16,7 +17,20 @@ class PrivacyPolicy extends StatelessWidget {
           builder: (context) {
             return Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text(privacyPolicy),
+              child: Column(
+                children: [
+                  kHeight10,
+                  const Text(
+                    "Privacy Policy",
+                    style:
+                        TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold),
+                  ),
+                  kHeight10,
+                  Text(privacyPolicy,
+                      style:
+                          const TextStyle(fontSize: 13.0, color: Colors.black)),
+                ],
+              ),
             );
           },
         );
